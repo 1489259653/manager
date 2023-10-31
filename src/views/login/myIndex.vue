01dc313ef99cc60cc5692c3ef133d1656b27f0f5
1489259653 <77714259+1489259653@users.noreply.github.com>
2023-10-31 09:30:31 +0800

    chore : fixed element ui

--- src/views/login/myIndex.vue
+++ src/views/login/myIndex.vue
@@ -1,165 +1,30 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="formRef"
-      :model="loginForm"
-      class="login-form"
-      autocomplete="on"
-      :rules="rules"
-    >
+
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
       <el-form-item>
         <el-icon><User /></el-icon>
-        <el-input v-model="loginForm.username"></el-input>
+        <el-input ></el-input>
       </el-form-item>
       <el-form-item >
         <el-icon><Lock /></el-icon>
         <el-input v-model="loginForm.password" :type="passwordType"></el-input>
       </el-form-item>
-      <el-form-item prop="username">
-        <el-icon><User /></el-icon>
-        <el-input v-model="loginForm.username"></el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-icon><Lock /></el-icon>
-        <el-input v-model="loginForm.password" :type="passwordType"></el-input>
-      </el-form-item>
-    </el-form>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-// import { User, Lock } from '@element-plus/icons-vue'
-// // import { useStore } from 'vuex'
-// import { loginRules } from './loginRule'
-// // const store = useStore()
-
+// import { User, Lock } from ' @element-plus/icons-vue'
 const loginForm = ref({
   username: 'admin',
   password: '123456'
 })
-// const rules = ref(loginRules)
-// const passwordType = ref('password')
-// // const formRef = ref(null)
-// const onChangePwdType = () => {
-//   passwordType.value === 'password'
-//     ? (passwordType.value = 'text')
-//     : (passwordType.value = 'password')
-// }
-// const handleLogin = async () => {
-//   formRef.value.validate((valid) => {
-//     if (valid) {
-//       store.dispatch('user/login', loginForm.value)
-//     } else {
-//       return false
-//     }
-//   })
-// }
+
 </script>
 
 <style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-$cursor: #fff;
 
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-
-    ::v-deep .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      color: #454545;
-    }
-
-    ::v-deep .el-input {
-      display: inline-block;
-      height: 47px;
-      width: 85%;
-
-      input {
-        background: transparent;
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
-        height: 47px;
-        caret-color: $cursor;
-      }
-    }
-    .login-button {
-      width: 100%;
-      box-sizing: border-box;
-    }
-  }
-
-  .tips {
-    font-size: 16px;
-    line-height: 28px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-
-    ::v-deep .lang-select {
-      position: absolute;
-      top: 4px;
-      right: 0;
-      background-color: white;
-      font-size: 22px;
-      padding: 4px;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-  }
-
-  .show-pwd {
-    // position: absolute;
-    // right: 10px;
-    // top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: (config) => {
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+    config.plugins.push(
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    )
+  }
+}
